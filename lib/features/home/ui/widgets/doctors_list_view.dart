5b40d5a238c8d5713e930af6996601fbaac98eb8
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2025-12-17 14:47:31 +0200

    added some changes

--- lib/features/home/ui/widgets/doctors_list_view.dart
+++ lib/features/home/ui/widgets/doctors_list_view.dart
@@ -21,22 +21,23 @@ class DoctorsListView extends StatelessWidget {
                     width: 110.w,
                     height: 110.h,
                     "assets/images/8c50e7b2c7369bd3e942c95803afc796ca283e34.png",
-
                     fit: BoxFit.cover,
                   ),
                 ),
-                SizedBox(width: 16.w,),
-               Expanded(
-                 child: Column(
-                   crossAxisAlignment: CrossAxisAlignment.start,
-                   children: [
-                     Text("Dr. Randy Wigham",style: textStyle16DarkBlueBold,),
-                     SizedBox(width: 8,),
-                     Text("General | RSUD Gatot Subroto",style: textStyle12grayMiddle,),
-
-                   ],
-                 ),
-               )
+                SizedBox(width: 16.w),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Dr. Randy Wigham", style: textStyle16DarkBlueBold),
+                      SizedBox(width: 8),
+                      Text(
+                        "General | RSUD Gatot Subroto",
+                        style: textStyle12grayMiddle,
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           );
